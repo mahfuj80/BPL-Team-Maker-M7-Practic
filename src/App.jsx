01d46c18a141772db6fda66a348218c38ad176a1
mahfuj80 <mahfujurrahman06627@gmail.com>
2023-09-14 18:38:06 +0600

    project resources and tailwind and daisy ui added

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <main className="bg-[url(./assets/images/Background_Galary.jpg)] w-[100-vh] h-[100vh] bg-cover bg-no-repeat bg-center object-cover object-center"></main>
+    </>
+  );
+}
+
+export default App;
